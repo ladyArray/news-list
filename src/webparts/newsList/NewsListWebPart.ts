d2a69d2aaea1e5194335dba2099b7a08e1a9c108
Regy <regina.rguez.c@gmail.com>
2022-10-25 15:02:00 +0200

    configs json: serve, imports

--- src/webparts/newsList/NewsListWebPart.ts
+++ src/webparts/newsList/NewsListWebPart.ts
@@ -1,25 +1,36 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
 
-import * as strings from 'NewsListWebPartStrings';
-import NewsList from './components/NewsList';
-import { INewsListProps } from './components/INewsListProps';
+import * as strings from "NewsListWebPartStrings";
+import NewsList from "./components/NewsList";
+import { INewsListProps } from "./components/INewsListProps";
+
+import { sp } from "@pnp/sp/presets/all";
+import {
+  PropertyFieldListPicker,
+  PropertyFieldListPickerOrderBy,
+} from "@pnp/spfx-property-controls";
+
+import {
+  ThemeProvider,
+  ThemeChangedEventArgs,
+  IReadonlyTheme,
+} from "@microsoft/sp-component-base";
 
 export interface INewsListWebPartProps {
+  title: string;
   description: string;
 }
 
 export default class NewsListWebPart extends BaseClientSideWebPart<INewsListWebPartProps> {
-
   private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
+  private _environmentMessage: string = "";
 
   public render(): void {
     const element: React.ReactElement<INewsListProps> = React.createElement(
@@ -29,7 +40,7 @@ export default class NewsListWebPart extends BaseClientSideWebPart<INewsListWebP
         isDarkTheme: this._isDarkTheme,
         environmentMessage: this._environmentMessage,
         hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
+        userDisplayName: this.context.pageContext.user.displayName,
       }
     );
 
@@ -43,11 +54,16 @@ export default class NewsListWebPart extends BaseClientSideWebPart<INewsListWebP
   }
 
   private _getEnvironmentMessage(): string {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams
-      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+    if (!!this.context.sdks.microsoftTeams) {
+      // running in Teams
+      return this.context.isServedFromLocalhost
+        ? strings.AppLocalEnvironmentTeams
+        : strings.AppTeamsTabEnvironment;
     }
 
-    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+    return this.context.isServedFromLocalhost
+      ? strings.AppLocalEnvironmentSharePoint
+      : strings.AppSharePointEnvironment;
   }
 
   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
@@ -56,16 +72,19 @@ export default class NewsListWebPart extends BaseClientSideWebPart<INewsListWebP
     }
 
     this._isDarkTheme = !!currentTheme.isInverted;
-    const {
-      semanticColors
-    } = currentTheme;
+    const { semanticColors } = currentTheme;
 
     if (semanticColors) {
-      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
-      this.domElement.style.setProperty('--link', semanticColors.link || null);
-      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+      this.domElement.style.setProperty(
+        "--bodyText",
+        semanticColors.bodyText || null
+      );
+      this.domElement.style.setProperty("--link", semanticColors.link || null);
+      this.domElement.style.setProperty(
+        "--linkHovered",
+        semanticColors.linkHovered || null
+      );
     }
-
   }
 
   protected onDispose(): void {
@@ -73,7 +92,7 @@ export default class NewsListWebPart extends BaseClientSideWebPart<INewsListWebP
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -81,20 +100,20 @@ export default class NewsListWebPart extends BaseClientSideWebPart<INewsListWebP
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
--- src/webparts/newsList/components/INewsListProps.ts
+++ src/webparts/newsList/components/INewsListProps.ts
@@ -1,7 +1,26 @@
+import { IReadonlyTheme } from "@microsoft/sp-component-base";
+
 export interface INewsListProps {
+  title: string;
   description: string;
+  category: string; //choice
+  publicationDate: Date;
+  responsable: string;
+  image: string;
+  storageList: string;
   isDarkTheme: boolean;
   environmentMessage: string;
   hasTeamsContext: boolean;
   userDisplayName: string;
+  themeVariant: IReadonlyTheme | undefined;
 }
+
+/*
+Crear una lista de “Noticias” con los siguientes campos:
+Título
+Descripción
+Categoría (elección)
+Fecha de publicación
+Responsable (*usuario)
+Imagen (*tipo vínculo)
+*/
